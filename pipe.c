047d99c9a7d93592d6c6f7587e1bfde54369ee92
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2022-01-22 15:38:27 -0500

    solucionado

--- pipe.c
+++ pipe.c
@@ -1,49 +1,79 @@
 #include <sys/types.h>
-#include <unistd.h>
-#include <stdio.h>
 #include <string.h>
-#include <errno.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdio.h>
+#include <wait.h>
+#include <time.h>
 
-#define READ  0
+#define READ 0
 #define WRITE 1
 
-int main() {
+//int argc, char const *argv[]
 
-  pid_t pid;//fork
-  int   fd[2];//Vector de posiciones
-  char *argv[] = {"/etc/hosts/",0}; 
-//0-Entrada Estandar
-  //1-Salida Estandar
-  if (pipe(fd) == -1) { //Funcion que permite comunicar 2 procesos 0-entrada 1-salida
-	perror("Creating pipe");
-	exit(EXIT_FAILURE);
-  }
+int main()
+{
+	printf("Init main \n");
+	srand(time(NULL));
 
-  switch(pid = fork()) {
+	pid_t pid; //fork
+	int fd[2]; //Vector de posiciones
+	int random_number;
+	char *argv[] = {"cat", "/etc/hosts", 0};
+	char *argv1[] = {"sort", "/etc/hosts", 0};
+	//0-Entrada Estandar
+	//1-Salida Estandar
 
-  case 0:
-	// The child process will execute wc.
-	// Close the pipe write descriptor.
-	close(fd[WRITE]);//
-	// Redirect STDIN to read from the pipe.
-	dup2(fd[READ], STDIN_FILENO); //Sobreescribir la salida estandar del hijo
-// Execute wc
-        //printf("Parent: %c", STDIN_FILENO);
-	execvp("sort",argv);
+	printf("Pipe create\n");
+	if (pipe(fd) == -1)
+	{
+		//Funcion que permite comunicar 2 procesos 0-entrada 1-salida
+		perror("Creating pipe \n");
+		exit(EXIT_FAILURE);
+	}
 
-  case -1:
-	perror("fork() failed)");
-	exit(EXIT_FAILURE);
+	//printf("Fork create \n");
 
-  default:
-	// The parent process will execute ls.
-	// Close the pipe read descriptor.
-	close(fd[READ]);
-	// Redirect STDOUT to write to the pipe.
-	dup2(fd[WRITE], STDOUT_FILENO);//Sobreescribir la salida estandar
-	// Execute ls -l
-	execvp("cat", argv);
-  }
+	pid = fork();
+
+	if (pid < 0)
+	{
+		perror("fork() failed");
+		exit(EXIT_FAILURE);
+	}
+
+	switch (pid)
+	{
+	case 0:
+		printf("Process hijo create\n");
+		// El proceso hijo ejecutará wc.
+		// Cierra el descriptor de escritura de pipe.
+		close(fd[WRITE]);
+		// Redirigir STDIN para leer desde el pipe.
+		dup2(fd[READ], STDIN_FILENO); //Sobreescribir la salida estandar del hijo
+		// Execute wc
+		// printf("Parent: %c", STDIN_FILENO);
+		execvp(argv1[0], argv1);
+		perror("Error inesperado de execvp()");
+		exit(EXIT_FAILURE);
+		break;
+
+	default:
+		// El proceso padre ejecutará ls.
+		// Cierra el descriptor de lectura de pipe.
+		//wait(NULL);
+		//random_number = rand() % 1000 + 1;
+
+		close(fd[READ]);
+		//Redirigir STDOUT para escribir en el pipe.
+		dup2(fd[WRITE], STDOUT_FILENO); //Sobreescribir la salida estandar
+		//Execute cat
+		execvp(argv[0], argv);
+		perror("Error inesperado de execvp()");
+		exit(EXIT_FAILURE);
+		break;
+	}
+
+	return 0;
 }
-
