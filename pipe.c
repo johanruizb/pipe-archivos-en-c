e40efdf3be4ed2fdc47bd33a332941579996614c
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2022-01-22 18:04:43 -0500

    add git ignore

--- pipe.c
+++ pipe.c
@@ -10,30 +10,30 @@
 #define READ 0
 #define WRITE 1
 
-//int argc, char const *argv[]
+// int argc, char const *argv[]
 
 int main()
 {
 	printf("Init main \n");
 	srand(time(NULL));
 
-	pid_t pid; //fork
-	int fd[2]; //Vector de posiciones
+	pid_t pid; // fork
+	int fd[2]; // Vector de posiciones
 	int random_number;
 	char *argv[] = {"cat", "/etc/hosts", 0};
 	char *argv1[] = {"sort", "/etc/hosts", 0};
-	//0-Entrada Estandar
-	//1-Salida Estandar
+	// 0-Entrada Estandar
+	// 1-Salida Estandar
 
 	printf("Pipe create\n");
 	if (pipe(fd) == -1)
 	{
-		//Funcion que permite comunicar 2 procesos 0-entrada 1-salida
+		// Funcion que permite comunicar 2 procesos 0-entrada 1-salida
 		perror("Creating pipe \n");
 		exit(EXIT_FAILURE);
 	}
 
-	//printf("Fork create \n");
+	// printf("Fork create \n");
 
 	pid = fork();
 
@@ -62,13 +62,13 @@ int main()
 	default:
 		// El proceso padre ejecutará ls.
 		// Cierra el descriptor de lectura de pipe.
-		//wait(NULL);
-		//random_number = rand() % 1000 + 1;
+		// wait(NULL);
+		// random_number = rand() % 1000 + 1;
 
 		close(fd[READ]);
-		//Redirigir STDOUT para escribir en el pipe.
+		// Redirigir STDOUT para escribir en el pipe.
 		dup2(fd[WRITE], STDOUT_FILENO); //Sobreescribir la salida estandar
-		//Execute cat
+		// Execute cat
 		execvp(argv[0], argv);
 		perror("Error inesperado de execvp()");
 		exit(EXIT_FAILURE);
