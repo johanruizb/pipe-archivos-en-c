84a08d8d59899744c57f2277ed06f33155b284ed
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2022-01-23 12:06:54 -0500

    añadir menor o mayor

--- pipe_3.c
+++ pipe_3.c
@@ -12,21 +12,17 @@
 
 int main()
 {
-    printf("Init main \n");
     srand(time(NULL));
 
     pid_t pid; // Fork
     int fd[2]; // Vector de posiciones
     int random_number;
-    char *argv[] = {"cat", "/etc/hosts", 0};
-    char *argv1[] = {"sort", "/etc/hosts", 0};
     char readbuffer[80];
     int nbytes;
 
     // 0-Entrada Estandar
     // 1-Salida Estandar
 
-    printf("Pipe create\n");
     if (pipe(fd) == -1)
     {
         // Funcion que permite comunicar 2 procesos 0-entrada 1-salida
@@ -50,7 +46,18 @@ int main()
         close(fd[WRITE]); // El proceso principal no necesita este extremo del pipe
 
         nbytes = read(fd[READ], readbuffer, sizeof(readbuffer));
-        printf("Read string: %s\n", readbuffer);
+        int numero = atoi(readbuffer);
+
+        if (numero >= 500)
+        {
+            printf("Numero mayor o igual a 500 (%d)\n", numero);
+        }
+        else if (numero < 500)
+        {
+            printf("Numero menor a 500 (%d)\n", numero);
+        }
+
+        //printf("Read string: %s\n", readbuffer);
 
         break;
 
