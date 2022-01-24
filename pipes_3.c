f766ee17db697c914f0fedbf364daf0134f8909a
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2022-01-24 15:27:20 -0500

    organizar codigo

--- pipes_3.c
+++ pipes_3.c
@@ -1,61 +1,83 @@
-#include <sys/stat.h>
+/*
+ * En este documento se describe el desarrollo de
+ * OS Tarea Pipe utilizando lo visto en clase.
+ * Emplearemos los temas Pipe y los archivos en Unix desde el lenguaje C.
+ *
+ * Codigo fuente: https://docs.google.com/document/d/1pnsiXOkwBmcnt0VFh1EEjMiodIcnXgejeT4BJcEjF2g/edit?usp=sharing
+ *
+ * Modificado por: Johan Ruiz - johan.andres.ruiz@correounivalle.edu.co
+ *                 Ingrid Echeverri - ingrid.echeverri@correounivalle.edu.co
+ * Fecha: 24/01/2022
+ */
+
 #include <sys/types.h>
-#include <fcntl.h>
-#include <assert.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <string.h>
-#include <errno.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdio.h>
 #include <time.h>
+
 #define READ  0
 #define WRITE 1
 
+ /*
+  * El proceso padre generará un númeo 
+  * entero aleatorio entre 1 y 1000, y
+  * este número será enviado a su hij. 
+  * El hijo imprimirá el mensaje “menor que 50” 
+  * si el número es menor que 500 y “mayor o igual a 50” 
+  * si el número es mayor o igual a 500.
+  *
+  * @param[out] int retorna 0, en caso de una ejecucion correcta
+ */
+
 int main() {
+        pid_t pid;
+        int fd[2];
+        char buffer[80];
+        srand(time(NULL));
 
-  pid_t pid;
-  int   fd[2];
-  char buffer[80];
-  srand(time(NULL));
-  if (pipe(fd) == -1) {
-        perror("Creating pipe");
-        exit(EXIT_FAILURE);
-  }
-
-  switch(pid = fork()) {
-
-  case 0: //Hijo
-
-        close(fd[WRITE]);
-
-        read(fd[READ], buffer, sizeof(buffer));
-        int num = atoi(buffer);
-        int file = open("resultado.txt", O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-        if(file >= 0){
-                if(num >= 500){
-                        sprintf(buffer, "%d es mayor o igual a 500\n", num);
-                }else{
-                        sprintf(buffer,"%d es menor que 500\n", num);
-                }
-                write(file, buffer, strlen(buffer));
-        }else{
-                perror("Error Archivo");
+        if (pipe(fd) == -1) {
+                perror("Creating pipe");
                 exit(EXIT_FAILURE);
         }
-        break;
 
+        switch (pid = fork()) {
 
-  case -1: //Error
+        case 0: //Hijo
 
-        perror("fork() failed)");
-        exit(EXIT_FAILURE);
+                close(fd[WRITE]);
 
-  default: ; //Padre
+                read(fd[READ], buffer, sizeof(buffer));
+                int num = atoi(buffer);
+                int file = open("resultado.txt", O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+                if (file >= 0) {
+                        if (num >= 500) {
+                                sprintf(buffer, "%d es mayor o igual a 500\n", num);
+                        }
+                        else {
+                                sprintf(buffer, "%d es menor que 500\n", num);
+                        }
 
-        int random_number = sprintf(buffer, "%d", rand() % 1000 + 1);
-        close(fd[READ]);
-        write(fd[WRITE], buffer, random_number);
-        break;
-}
+                        printf("Registro de respuesta disponible en 'respuesta.txt'. Usa: \n\t> cat respuesta.txt");
+                        write(file, buffer, strlen(buffer));
+                }
+                else {
+                        perror("Error Archivo");
+                        exit(EXIT_FAILURE);
+                }
+                break;
+
+        case -1: //Error
+                perror("fork() failed");
+                exit(EXIT_FAILURE);
+        default:; //Padre
+                int random_number = sprintf(buffer, "%d", rand() % 1000 + 1);
+                close(fd[READ]);
+                write(fd[WRITE], buffer, random_number);
+                break;
+        }
         return 0;
 }
